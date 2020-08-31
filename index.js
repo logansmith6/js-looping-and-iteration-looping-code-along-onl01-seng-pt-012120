bc899d785e46ddc370eb56d2f8674e85c26d30a3
Logan Smith <logsmith06@gmail.com>
2020-08-31 23:54:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 let thanks = [];
 function writeCards(cards){
   for(let x = 0; x < cards.length; x++) {
-    thanks.unshift("Thank you, ${cards[i]}");
+    thanks.unshift(console.log("Thank you, ${cards[i]}"));
   }
   return thanks;
 }
