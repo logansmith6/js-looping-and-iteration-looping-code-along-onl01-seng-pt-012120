c550290c6d5f48cd448469307bd0c602e814bce4
Logan Smith <logsmith06@gmail.com>
2020-09-01 00:10:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+let thanks = [];
+function writeCards(cards){
+  for(let x = 0; x < cards.length; x++) {
+   thanks[x] = 'Thank you, ' + cards[x] + ', for the wonderful surprise gift!';
+  }
+  return thanks;
+}
+
+function countDown(num){
+  for(let x = 0; x <= num; num--) {
+    console.log(num);
+
+  }
+
+}
