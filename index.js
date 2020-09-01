fc1f82d0aa336b658871860c8f2356b3930c85b6
Logan Smith <logsmith06@gmail.com>
2020-09-01 00:01:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solutions in this file
-let thanks = [];
+
 function writeCards(cards){
   for(let x = 0; x < cards.length; x++) {
     let thanks[i] = 'Thank you, ${cards}, for the wonderful surprise gift!'
